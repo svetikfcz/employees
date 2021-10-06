91cd96acc0ee4542dbdd8775e067e83bc7f62880
Svetlana Efimova <svetlana.e08@gmail.com>
2021-10-06 13:07:09 +0300

    props

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,13 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+
+    const data = [
+        {name: 'Максим В.', salary: 800, increase: true},
+        {name: 'Ольга А.', salary: 3000, increase: true},
+        {name: 'Ярослав П.', salary: 4000, increase: false},
+    ];
+
     return (
         <div className="app">
             <AppInfo />
@@ -15,7 +22,7 @@ function App() {
                 <AppFilter />
             </div>
 
-            <EmployeesList />
+            <EmployeesList data={data} />
             <EmployeesAddForm />
         </div>
     );
